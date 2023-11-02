da4551fbb67cba992567a87282301aa41eb50e5b
Xander <XanderG2@users.noreply.github.com>
2023-11-02 16:58:46 +0000

    fix spelling

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 aliens = 2
 password = "ALIENS"
-print("Qickly! aliens are invading  the planet.")
-print("you need to actiate the global platforms")
-print("Hope you now the passward,for earth's sake")
+print("Quickly! Aliens are invading  the planet.")
+print("You need to actiate the global platforms.")
+print("Hope you know the password, for Earth's sake.")
 print()
 print("---------------------------------------------------------")
 print ("       Welcome to the global defence network             ")
 print ("---------------------------------------------------------")
 print ()
-guess = input(" Please enter the password:").upper()
+guess = input("Please enter the password: ").upper()
 while guess != password:
     print ()
-    print ("Increct password")
+    print ("Incorrect password.")
     print ()
     aliens = aliens ** 2
-    print("There",aliens,"aliens now on Earth. try again!")
+    print("There are",aliens,"aliens now on Earth. Try again!")
     if aliens > 7400000000:
         break
     print ()
-    print ("password hint: the things that are attaching us ")
+    print ("Password hint: the things that are attacking us.")
     print ()
-    guess = input(" Please enter the password:"). upper()
+    guess = input("Please enter the password: "). upper()
     if aliens>7400000000:
-        print("noooooooooo! the allons out nummberd us all is loost")
+        print("Noooooooooo! the aliens out numbered us! All is lost!")
     else:
-        print ("hooray! we won the would is saved")
+        print ("Hooray! We won; the world is saved!")
